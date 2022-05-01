c5195a88a2f1a121b453157efc1730b730c45222
soulslurpee <charles.d.thompson87@gmail.com>
2022-05-01 19:34:05 -0400

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,31 @@
+INSERT INTO department
+  (name)
+VALUES
+  ('Finance'),
+  ('Development'),
+  ('Legal'),
+  ('Sales');
+
+INSERT INTO role
+  (title, salary, department_id)
+VALUES
+  ('Comptroller', 80000, 1),
+  ('Accountant', 50000, 1),
+  ('Project Manager',250000, 2),
+  ('Engineer', 125000, 2),
+  ('General Council', 350000, 3),
+  ('Paralegal', 50000, 3),
+  ('Sales Lead', 90000, 4),
+  ('Sales Rep', 60000, 4);
+
+INSERT INTO employee 
+  (first_name, last_name, role_id, manager_id)
+VALUES
+  ('Johnson', 'Scott', 1, NULL),
+  ('Richards', 'Dave', 2, 1),
+  ('Gonzales', 'Rose', 3, NULL),
+  ('Thompson', 'Charles', 4, 2),
+  ('Leon', 'Mary', 5, NULL),
+  ('Gerbert', 'Linda', 6, 3),
+  ('Holmes', 'Sherlock', 7, NULL),
+  ('White', 'Betty', 8, 4);
