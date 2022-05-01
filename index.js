68a5496c026864b4bbe8f5ab09b04f37be3b60af
soulslurpee <charles.d.thompson87@gmail.com>
2022-05-01 19:51:57 -0400

    added mainMenu function

--- index.js
+++ index.js
@@ -16,9 +16,68 @@ db.connect((err) => {
     console.log(err);
     throw err;
   }
-  console.log('MySQL connection established')
+  console.log('<=Connection to Employee Database=>');
+  mainMenu();
 });
 
+function mainMenu() {
+  inquirer.prompt([
+  {
+  type: "list",
+  message: "Please select from the following options:",
+  name: "choice",
+  choices: [
+            "[View All Employees]", 
+            "[Update Employee]",
+            "[Add Employee]",
+            "[View All Employees By Roles]",
+            "[Add Role]",
+            "[View all Employees By Deparments]", 
+            "[Add Department]"
+          ]
+  }
+]).then((result) => {
+      switch (result.choice) {
+        case "[View All Employees]":
+          viewAllEmployees();
+        break;
+
+        case "[Update Employee]":
+          updateEmployee();
+        break;
+
+        case "[Add Employee]":
+          addEmployee();
+        break;
+  
+        case "[View All Employee's By Roles]":
+          viewAllRoles();
+        break;
+
+        case "[Add Role]":
+          addRole();
+        break;
+
+        case "[View all Emplyees By Deparments]":
+          viewAllDepartments();
+        break;
+
+        case "[Add Department]":
+          addDepartment();
+        break;
+      }
+  })
+}
+
+function viewAllEmployees() {
+  connection.query("SELECT employee.id, employee.first_name, employee.last_name, role.title AS role FROM employee LEFT JOIN role ON employee.role_id = role.id", 
+  (err, res) => {
+    if (err) 
+    throw err
+    console.table(res)
+    startPrompt()
+})
+}
 
 
 app.listen(PORT, () => {
